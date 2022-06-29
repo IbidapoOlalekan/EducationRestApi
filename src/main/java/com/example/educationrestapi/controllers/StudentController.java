85381e916d5e6ad208843b6e967a285e39601c2f
Ibidapo Abdulazeez Olalekan <ibidapoazeez@gmail.com>
2022-06-29 22:39:11 +0100

    Updated Commit

---
+++ src/main/java/com/example/educationrestapi/controllers/StudentController.java
@@ -0,0 +1,28 @@
+package com.example.educationrestapi.controllers;
+
+import com.example.educationrestapi.dtos.requests.CreateStudentRequest;
+import com.example.educationrestapi.exceptions.StudentException;
+import com.example.educationrestapi.services.StudentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("api/v1/EducationApi/")
+public class StudentController {
+    private final StudentService studentService;
+
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @PostMapping("createAccount")
+    public ResponseEntity<?> createStudentAccount(@RequestBody CreateStudentRequest createStudentRequest) {
+        try {
+            return new ResponseEntity<>(studentService.createAccount(createStudentRequest), HttpStatus.CREATED);
+        }
+        catch(StudentException e) {
+            return new ResponseEntity<>(e.getMessage(),HttpStatus.BAD_REQUEST);
+        }
+    }
+}
